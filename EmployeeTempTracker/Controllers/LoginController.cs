125a307afca78942284cf0579bc01406a92fceab
Jacky <lu.jacky380@gmail.com>
2020-10-21 13:41:27 -0400

    added skeleton logic for user id check, needs update when api available

--- EmployeeTempTracker/Controllers/LoginController.cs
+++ EmployeeTempTracker/Controllers/LoginController.cs
@@ -28,12 +28,42 @@ namespace EmployeeTempTracker.Controllers {
         // Changing this to return an IActionResult (return View();) would search for /Views/Login/AuthUser.cshtml
         // Get http://capstone.ohitski.org/Login/AuthUser
         public IActionResult AuthUser(string uname, string passwd, int id = 1) {
+            ViewData["Title"] = "Authenticate";
             // Handle user login API call here, unless it will be implemented in APIController.cs
             
-            ViewData["Title"] = "Authenticate";
+            //Will use something like below code when we have API/login calls available to compare input username/password to id database:
+
+            // var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();  
+            // if (obj != null){  
+            //     Session["UserID"] = obj.UserId.ToString();  
+            //     Session["UserName"] = obj.UserName.ToString();  
+            //     return RedirectToAction("UserDashBoard");  
+            // } 
+
+            Boolean someLogicToCheckDataBaseForUser = true; //TEMPORARY FIELD, REMOVE WHEN API CALLS ARE IMPLEMENTED
+            
+            if(someLogicToCheckDataBaseForUser){
+                return RedirectToAction("UserDashboard",new{uname = uname,passwd = passwd, id = id});
+            }
+            else{
+                return RedirectToAction("InvalidLogin");
+            }
+
+            return View();
+        }
+
+        public IActionResult UserDashBoard(string uname, string passwd, int id = 1){
+            ViewData["Title"] = "Dashboard";
             ViewData["Message"] = HtmlEncoder.Default.Encode($"Hello, {uname}, your id is {id} and you entered {passwd} as your password.");
             return View();
         }
+
+        public IActionResult InvalidLogin(){
+            ViewData["Title"] = "InvalidLogin";
+            ViewData["Message"] = HtmlEncoder.Default.Encode($"Invalid username/password.");
+            return View();
+        }
+
 
     }
 }
