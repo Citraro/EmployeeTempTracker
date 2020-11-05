1c3e33315464a0decf236580637cc9250759fed0
David Garlak <dgarlak@gmail.com>
2020-11-05 10:49:34 -0500

    Constructors for LoginModel - help reuse code

--- EmployeeTempTracker/Models/LoginModel.cs
+++ EmployeeTempTracker/Models/LoginModel.cs
@@ -18,5 +18,24 @@ namespace EmployeeTempTracker.Models
         public bool RememberMe { get; set; }
         public bool SessionValid { get; set; }
         public string SessionId { get; set; }
+
+        public LoginModel() {
+            DomainName =    null;
+            Username =      null;
+            Password =      null;
+            RememberMe =    false;
+            SessionValid =  false;
+            SessionId =     null;   
+        }
+
+        public LoginModel(string domain, string user, string pass) {
+            DomainName =    domain;
+            Username =      user;
+            Password =      pass;
+            RememberMe =    false;
+            SessionValid =  false;
+            SessionId =     null;
+        }
     }
+
 }
